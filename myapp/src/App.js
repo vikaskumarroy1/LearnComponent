07cfe1c28e50fb6c3a8146a94f2a24bb5049ea48
classwwork <vikasroy1307@gmail.com>
2023-12-17 14:15:01 +0530

    magase-parler

---
+++ myapp/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import Nav from './component/Nav/Nav';
+import Main from './component/Main/Main';
+import Footer from './component/Footer/Footer'
+
+
+
+
+
+function App() {
+  
+  return (
+    <div>
+  <Nav/>
+  <Main/>
+  <Footer/>
+  </div>
+   
+
+  );
+}
+
+export default App;
